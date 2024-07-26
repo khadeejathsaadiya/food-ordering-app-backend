a466354deb8e69b5e4a618e69fd9a00dd61b3176
khadeejath <saadiyakhadeejath10@gmail.com>
2024-07-27 00:17:34 +0530

    added restaurant search functions

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import { v2 as cloudinary } from "cloudinary";
-
+import restaurantRoute from "./routes/RestaurantRoute";
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => console.log("Connected to database!"));
@@ -28,6 +28,7 @@ app.get("/health", async (req: Request, res: Response) => {
 //  /api/my/user
 app.use("/api/my/user", myUserRoute)
 app.use("/api/my/restaurant", myRestaurantRoute)
+app.use("/api/restaurant", restaurantRoute)
 
 app.listen(7000, () => {
     console.log("server started on localhost:7000")
