02684d1ed9d6bd85cdf671e698fd19767b4c54be
khadeejath <saadiyakhadeejath10@gmail.com>
2024-07-29 17:32:14 +0530

    added restaurant and order features

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import { v2 as cloudinary } from "cloudinary";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
+
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => console.log("Connected to database!"));
@@ -29,6 +31,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute)
 app.use("/api/my/restaurant", myRestaurantRoute)
 app.use("/api/restaurant", restaurantRoute)
+app.use("/api/order", orderRoute);
 
 app.listen(7000, () => {
     console.log("server started on localhost:7000")
